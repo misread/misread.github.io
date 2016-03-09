41cb50493a6e1e5d9015c85214df70679f2dfd0b
Brandon B <unaffiliate@outlook.com>
2016-03-08 20:54:34 -0500

    minimalism will kill me.

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function() {
-    $("#fullpage").fullpage({
-      anchors:["welcome", "photos", "social", "snapchat"],
-      css3: true,
-    });
-    jQuery("#title").fitText();
-});
